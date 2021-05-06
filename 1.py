28c80870dbcaf5db91598e7622ce84ff43c0c08d
전혜은 <83629720+jeonhyeeun@users.noreply.github.com>
2021-05-06 16:33:23 +0900

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+def chat(name1, name2, age):
+    print("%s : 안녕? 넌 몇살이니?" %name1)
+    print("%s : 나? 나는 %d" %(name2, age) +"살이야!")
+
+a = "정수"
+b = "태희"
+c = "환준"
+d = "세영"
+
+chat(a, b, 20)
+chat(c, d, 19)
