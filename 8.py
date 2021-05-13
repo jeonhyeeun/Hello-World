e54c78b9b813a21ca39197075e2bdd378d2a2e4a
전혜은 <83629720+jeonhyeeun@users.noreply.github.com>
2021-05-14 00:00:18 +0900

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,27 @@
+x = 2
+y = 1
+z = 0
+
+if x > y :
+    print("x(2)는 y(1)보다 크다.")
+print("x가 y보다 크기(Ture)때문에 출력된다.", "\n")
+
+if x < y :
+    print("x보다 y가 크다.")
+print("y가 x 보다 크기 않기(False)때문에 출력이 안된다.", "\n")
+
+if not x < y :
+    print("x보다 y가 크지 않다.")
+print("not이 붙어서 Ture이기 때문에 출력된다.", "\n")
+
+if x > z and x > y :
+    print("y가 z보다 크고 x가 y보다 크다.")
+print("두가지 모두 맞으면 (Ture)출력한다.", "\n")
+
+if x > y :
+    print("x(2)는 y(1)보다 크다.")
+print("x가 y보다 크기(Ture)때문에 출력된다", "\n")
+
+if x > y :
+    print("x(2)는 y(1)보다 크다.")
+print("x가 y보다 크기(Ture)때문에 출력된다", "\n")
