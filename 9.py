1671a413ef147fe74509a67829f3403242593bfc
전혜은 <83629720+jeonhyeeun@users.noreply.github.com>
2021-05-15 21:52:28 +0900

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,14 @@
+# 블리언 3rd - 조건문 if else, elif, else
+a = 3
+b = 2
+c = 5
+d = c - a
+
+if a > b :                        # a가 b보다 크면 Hello 0_0
+    print("Hello 0_0")
+if a > c :                        # a가 c보다 크면 What?
+    print("What?")
+elif a == d :                     # a가 3과 같다면 Bye~!
+    print("Bye~!")
+else:                             # a가 b보다 작으면 Hi
+    print("Hi!")
